e5c6f92c84a80e81d44a28de9ea0f57294af2f5a
vpavlenko <cxielamiko@gmail.com>
2013-11-03 20:08:20 +0400

    3 nov: remove duplicated code

--- uwsgi-vps-deployment/flaskhello.py
+++ uwsgi-vps-deployment/flaskhello.py
@@ -1,4 +1,4 @@
-from flask import Flaskapp = Flask(__name__)@app.route('/<address>')def hello_world(address):    return 'Hello World! Called with ' + addressfrom flask import Flask
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/<address>')
